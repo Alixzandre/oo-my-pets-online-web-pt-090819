8b3a71133eea165a7849addb0eaa54e01524f901
Alexander Bispo <alexbispo173@gmail.com>
2019-10-24 01:47:35 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,12 @@
 class Owner
   @@all = []
-  
+  @@count = 1
   attr_reader :name, :species
   def initialize(name,species="human")
     @species = species
     @name = name
     @@all << self
+    @@count +=1
   end
 
   def say_species
@@ -15,5 +16,7 @@ class Owner
   def self.all
     @@all
   end
+  
+  
 
 end
