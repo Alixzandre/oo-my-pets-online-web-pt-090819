1730346c4ffa08e9e25de8d462e481b474d3931b
Alexander Bispo <alexbispo173@gmail.com>
2019-10-24 01:58:22 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
   attr_reader :name
-  attr_accessor :owner
+  attr_accessor :owner, :mood
 
-  def initialize(name,owner)
+  def initialize(name,owner,mood = "nervous")
     @owner = owner
     @name = name
+    @mood = mood
   end
 
 end
